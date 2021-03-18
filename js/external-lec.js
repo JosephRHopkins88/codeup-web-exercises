0eba1d81be7b15302a61bfe40f95d90b35fab837
Joseph Hopkins <joseph.r.hopkins88@gmail.com>
2021-03-18 15:28:33 -0500

    completed the js with html lecture

---
+++ js/external-lec.js
@@ -0,0 +1,26 @@
+"use strict";
+
+var message = "Howdy Y'all!";
+
+console.log(message);
+
+var favoriteCheese = prompt ("What is your favorite cheese?");
+
+console.log(favoriteCheese + " happens to be their favorite chesse. Huzzah!");
+
+var num = parseFloat(prompt("Gimme a number..."));
+
+console.log("User entered: " + num +". That number plus 7 is : " + (num +17) + ".");
+
+var canIGoToToysRUs = confirm("Can I go to Toys 'R Us, please?");
+
+console.log(canIGoToToysRUs);
+
+// Javacript Comment
+
+/**
+ *
+ * Block Comment
+ *
+ */
+
