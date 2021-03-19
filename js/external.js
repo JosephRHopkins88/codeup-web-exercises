e6047ed0197baaa464ccc5a973f8cedf48faddb6
Joseph Hopkins <joseph.r.hopkins88@gmail.com>
2021-03-19 11:30:25 -0500

    completed the exercise walkthrough for js with html

---
+++ js/external.js
@@ -0,0 +1,46 @@
+"use strict";
+
+//var message = "Hello from external JavaScript.";
+
+//console.log(message);
+
+//alert("Welcome to my Website!");
+
+//var userInput = prompt("What is your favorite color?");
+
+//alert("Great, " +  userInput + " is my favorite color too.");
+
+// Exercise 3.1
+
+//var theLittleMermaid = prompt("How many days did you rent The Little Mermaid?");
+
+//var brotherBear = prompt("How many days did you rent Bother Bear?");
+
+//var hercules = prompt("How many days did you rent Hercules?");
+
+//var total = (theLittleMermaid * 3) + (brotherBear * 3) + (hercules * 3)
+
+//alert("Great! Your total is $" + total)
+
+// Exercise 3.2
+
+//var google = prompt("How many hours did you work at Google?");
+
+//var amazon = prompt("How many hours did you work at Amazon?");
+
+//var facebook = prompt("How many hours did you work at Facebook?");
+
+//var total = (google * 400) + (amazon * 380) + (facebook * 350);
+
+//alert("Great! Your total payment this week is $" + total);
+
+// Exercise 3.3
+
+//confirm("Press \"OK\" if there is room in the class otherwise press \"Cancel\"")?(confirm("Press \"OK\" if this class does not conflict with current schedule, otherwise press \"Cancel\"")?alert("You may register for this class!"):alert("You may NOT register for this class!")):alert("You may NOT register for this class!");
+
+// Exercise 3.4
+
+//confirm("Press \"OK\" if you are purchasing 2 or more items, otherwise press \"Cancel\"")?alert("Offer is valid."):(confirm("Press \"OK\" if you are a Premium Member, otherwise press \"Cancel\".")?alert("Offer is valid."):alert("Offer is NOT valid."));
+
+
+
