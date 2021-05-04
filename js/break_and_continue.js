4d97757eec1f0625b7f7319ec0bff0cef5a64798
Joseph Hopkins <joseph.r.hopkins88@gmail.com>
2021-05-04 14:35:52 -0500

    fixed

--- js/break_and_continue.js
+++ js/break_and_continue.js
@@ -4,12 +4,10 @@ var oddNumber = 0;
 
 while (oddNumber !== "You are never going to come up with this.") {
     oddNumber = parseFloat(prompt("Please enter a number between 1 and 50."));
-    if (oddNumber % 2 === 1 || (oddNumber > 0 && oddNumber < 50)) {
-        break;
-    }
+    if (oddNumber % 2 === 1 || (oddNumber > 0&& oddNumber < 50)) {break;}
 }
 
-for (var i = 1; i < 50, i += 2) {
+for (var i = 1; i < 50, i+=2;) {
     if (i === oddNumber) {
         console.log("Yikes! Skipping number : " + oddNumber);
         continue;
